4d383d33de3e2b75337f3ca80c363659f9ba949e
Jeandson Barros <61754115+JeandsonBarros@users.noreply.github.com>
2023-07-03 01:38:12 -0300

    pronto

---
+++ Formação .NET Developer/exemplos/C# basico, manipulação de dados e orientação a objetos/Herança e Polimorfismo/Models/Aluno.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Aluno: Pessoa
+    {
+       public  double Nota { get; set; } 
+
+         public override void Apresentar(){
+            System.Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e minha nota é {Nota}");
+        }
+    }
+}
---
+++ Formação .NET Developer/exemplos/C# basico, manipulação de dados e orientação a objetos/Herança e Polimorfismo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+
+//Exemplo que usa o polimofismo(override) e herança
+Aluno aluno1 = new Aluno();
+aluno1.Nome ="Jeu";
+aluno1.Idade = 25;
+aluno1.Nota = 10;
+aluno1.Apresentar();
+
+//Exemplo que usa herança
+Professor professor1 = new Professor();
+professor1.Nome ="Zoro";
+professor1.Idade = 25;
+professor1.Salario = 500;
+professor1.Apresentar();
